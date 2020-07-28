2e9968f3d6e520a183bdb5e7f0c63cac92cc6a6d
adam-osu <kasho.dev@gmail.com>
2020-07-27 21:07:46 -0400

    Add sign in user mutation, add test

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,4 +1,5 @@
 module Mutations
+  # This class is used as a parent for all mutations, and it is the place to have common utilities
   class BaseMutation < GraphQL::Schema::Mutation
     null false
   end
