a348a0133926005840521cb2d976c2c1bc5e2f94
adam-osu <kasho.dev@gmail.com>
2020-07-28 00:25:52 -0400

    Remove old comment

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,5 @@
 module Types
   class QueryType < Types::BaseObject
-    # queries are just represented as fields
-    # `all_links` is automatically camelcased to `allLinks`
-    # field :all_links, [LinkType], null: false
-
     field :all_links, [LinkType], resolver: Resolvers::LinksSearch, null: false
 
     # this method is invoked, when `all_link` fields is being resolved
